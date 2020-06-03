18e517451bab8e55ddaa466791710fa4bdde1012
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 15:18:51 +0900

    문제 다시풀기

--- src/com/javaex/practice02/Ex06.java
+++ src/com/javaex/practice02/Ex06.java
@@ -10,7 +10,10 @@ public class Ex06 {
 		
 		int num1;
 		int num2;
-				
+		int max = 0;
+		int min = 0;
+		
+		//1.숫자 입력하기
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자2개를 입력해주세요");
 		System.out.print("숫자1: ");
@@ -18,19 +21,26 @@ public class Ex06 {
 		System.out.print("숫자2: ");
 		num2 = sc.nextInt();
 		
-		
+		//2.두 수의 크기차이 구별하기
 		if(num1 > num2) {
-			System.out.println("큰수: "+num1+"\t"+"작은수: "+num2+" 입니다.");
+			max = num1;
+			min = num2;
 		}
 		
 		else if(num1 < num2) {
-			System.out.println("큰수: "+num2+"\t"+"작은수: "+num1+" 입니다.");
+			max = num2;
+			min = num1;
 		}
 		
 		else {
-			System.out.println("큰수: "+num1+"\t"+"작은수: "+num2+" 입니다.");
+			System.out.println("두 수는 같습니다.");
+			max = num1;
+			min = num2;
 		}
-			
+		
+		//3.결과 출력
+		System.out.println("큰수: "+max+"\t"+"작은수: "+min+" 입니다.");
+		
 		
 		sc.close();
 		
