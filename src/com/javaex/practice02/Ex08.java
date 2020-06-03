0f9a8ca3b45de5303b5294ecad18286685c565b6
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 15:41:21 +0900

    문제 다시풀기

--- src/com/javaex/practice02/Ex08.java
+++ src/com/javaex/practice02/Ex08.java
@@ -9,6 +9,9 @@ public class Ex08 {
 		//가장 작은 수 구하기
 		
 		int num1, num2, num3;
+		int min;
+		
+		//1.숫자 입력하기
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자 3개를 입력해주세요");
 		System.out.print("숫자1: ");
@@ -18,18 +21,18 @@ public class Ex08 {
 		System.out.print("숫자3: ");
 		num3 = sc.nextInt();
 		
-		if(num1>num2) {
-			if(num2> num3) {
-				System.out.println("가장 작은수는 "+num3+" 입니다.");}
-			else {System.out.println("가장 작은수는 "+num2+" 입니다.");}
+		//2.제일 작은수 가려내기
+		min = num1;
+		if(min>num2) {
+			min = num2;
 		}
 		
-		else {
-			if(num1>num3) {
-				System.out.println("가장 작은수는 "+num3+" 입니다.");
-			}
-			else {System.out.println("가장 작은수는 "+num1+" 입니다.");}
+		if(min>num3) {
+			min = num3;
 		}
+		
+		//3.출력
+		System.out.println("가장 작은수는 "+min+" 입니다.");
 		sc.close();
 		
 		
